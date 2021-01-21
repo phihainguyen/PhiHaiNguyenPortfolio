e3172d8ef1336379296fcff32f72294982bcc286
phihainguyen <phihai93@gmail.com>
2021-01-21 09:45:30 -0800

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar';
+import './styles.css';
 const App = () => {
 	return (
 		<div>
+			<Navbar />
 			<h1>APP</h1>
 		</div>
 	);
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Navbar
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<FontAwesomeIcon icon={faBars} style={{ color: 'red' }} />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+						<li className="nav-item active">
+							<a className="nav-link " aria-current="page" href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link " aria-current="page" href="#">
+								About Me
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link " aria-current="page" href="#">
+								Portfolio
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link " aria-current="page" href="#">
+								Contact
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Link
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
